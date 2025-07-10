c78a1cd0228c69fdbe564e02e9d3416fe36b5afd
HoGoodDev <hogood.dev@gmail.com>
2025-07-09 22:05:35 -0400

    Add project files.

---
+++ Binary Converter/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim binaryString As String = "1011011"
+        Dim decimalString As String = ConvertBintoDec(binaryString)
+        Console.WriteLine($"Binary {binaryString} is decimal {decimalString}")
+        Console.ReadLine()
+    End Sub
+
+    Function ConvertBintoDec(bnum As String) As String
+        Dim totalnum As Integer = 0
+
+        For i As Integer = 0 To bnum.Length - 1
+            If bnum(i) = "1"c Then
+                Dim bit As Integer = 2 ^ (bnum.Length - 1 - i)
+                totalnum += bit
+            ElseIf bnum(i) = "0"c Then
+
+            Else
+                Throw New ArgumentException("Input contains invalid characters.")
+            End If
+        Next
+
+        Return totalnum.ToString()
+    End Function
+
+End Module
